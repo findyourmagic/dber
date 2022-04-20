a860837571e54cca60f950436583fb12dcf279f1
findyourmagic <findyourmagic@foxmail.com>
2022-04-20 15:58:01 +0800

    [update] delete link

--- components/link_modal.js
+++ components/link_modal.js
@@ -5,11 +5,47 @@ import {
     Select,
     Space,
     Notification,
+    Popconfirm,
 } from '@arco-design/web-react';
 import { useEffect, useState, useMemo } from 'react';
 
 export default function LinkModal(props) {
-    const { editingLink, setEditingLink } = props;
+    const { editingLink, setEditingLink, setLinkDict } = props;
+    const changeRelation = relation => {
+        const { linkId, fieldId } = editingLink;
+        setLinkDict(state => {
+            return {
+                ...state,
+                [linkId]: {
+                    ...state[linkId],
+                    endpoints: state[linkId].endpoints.map(endpoint => {
+                        if (endpoint.fieldId == fieldId) {
+                            return {
+                                ...endpoint,
+                                relation,
+                            };
+                        }
+                        if (relation == '*' && endpoint.fieldId != fieldId) {
+                            return {
+                                ...endpoint,
+                                relation: '1',
+                            };
+                        }
+                        return endpoint;
+                    }),
+                },
+            };
+        });
+        setEditingLink(null);
+    };
+    const removeLink = () => {
+        const { linkId, fieldId } = editingLink;
+        setLinkDict(state => {
+            delete state[linkId];
+            return { ...state };
+        });
+        setEditingLink(null);
+    };
     return (
         <Modal
             title="Link"
@@ -19,11 +55,39 @@ export default function LinkModal(props) {
             autoFocus={false}
             focusLock={false}
         >
-            <Space direction="vertical">
-                <Button long>1:1</Button>
-                <Button long>1:N</Button>
-                <Button long>N:1</Button>
-                <Button long>Delete Path</Button>
+            <Space
+                style={{
+                    width: '100%',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <Space>
+                    <label>Change relation:</label>
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                            changeRelation('1');
+                        }}
+                    >
+                        1
+                    </Button>
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                            changeRelation('*');
+                        }}
+                    >
+                        *
+                    </Button>
+                </Space>
+                <Popconfirm
+                    title="Are you sure to delete this path?"
+                    onOk={() => {
+                        removeLink();
+                    }}
+                >
+                    <Button>Delete Path</Button>
+                </Popconfirm>
             </Space>
         </Modal>
     );
