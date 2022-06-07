821851dcf7364a6a01f9fe266e3715b47a147c58
findyourmagic <findyourmagic@foxmail.com>
2022-06-07 22:59:51 +0800

    [update] import modal

--- components/import_modal.js
+++ components/import_modal.js
@@ -10,6 +10,41 @@ export function ImportModal({ importType, setImportType, addGraph }) {
         try {
             const graph = await Parser.parse(value, importType.toLowerCase());
             console.log(graph);
+            const tableDict = {};
+            const linkDict = {};
+            graph.tables.forEach(table => {
+                const id = window.crypto.randomUUID();
+                tableDict[id] = {
+                    ...table,
+                    id,
+                    fields: table.fields.map(field => {
+                        const fieldId = window.crypto.randomUUID();
+                        return {
+                            ...field,
+                            id: fieldId,
+                            type: field.type.type_name
+                        }
+                    })
+                }
+            })
+
+            graph.refs.forEach(ref => {
+                const id = window.crypto.randomUUID();
+                linkDict[id] = {
+                    ...ref,
+                    id,
+                    endpoints: ref.endpoints.map(endpoint => {
+                        const table = Object.values(tableDict).find(table=> table.name == endpoint.tableName);
+                        return {
+                            id: table.id,
+                            fieldId: table.fields.find(field => field.name == endpoint.fieldNames[0]).id
+                        }
+                    })
+                }
+            });
+
+            console.log(tableDict);
+            console.log(linkDict);
             // addGraph(graph);
             setValue('');
             setImportType('');
