b198fb01e4d224b735d3c0e12a9729516f99f930
findyourmagic <findyourmagic@foxmail.com>
2022-06-30 18:06:51 +0800

    [update] landing page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const withSvgr = require('next-plugin-svgr');
+
+const nextConfig = withSvgr({
     reactStrictMode: true,
     poweredByHeader: false,
-};
+});
+
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ import {
     PageHeader,
     Space,
     Typography,
+    Steps,
 } from '@arco-design/web-react';
+
+const Step = Steps.Step;
+
+import IconGithub from '../public/images/github.svg';
 
 export default function Home() {
     return (
@@ -17,6 +22,7 @@ export default function Home() {
                     content="Database design tool based on entity relation diagram"
                 />
                 <link rel="icon" href="/favicon.ico" />
+                <style>{'body { overflow: auto !important; }'}</style>
             </Head>
             <div className="index-container">
                 <PageHeader
@@ -26,30 +32,85 @@ export default function Home() {
                     extra={
                         <Space>
                             <Button href="/graphs" type="primary">
-                                Get started
+                                Get started free & no registration required.
                             </Button>
-                            <Button type="primary">Github</Button>
+                            <Button
+                                type="primary"
+                                icon={<IconGithub className="arco-icon" />}
+                                style={{ backgroundColor: '#333' }}
+                            >
+                                Github
+                            </Button>
                         </Space>
                     }
                 />
                 <div className="index-bg">
-                    <Typography.Title className="tc">
+                    <Typography.Title className="tc" type="secondary">
                         <p>Simply easy</p>
-                        <strong>Data modeling & Database designing</strong>
-                        <p>Open source tool for free.</p>
+                        <p className="mark">
+                            Database designing & Data modeling tool
+                        </p>
+                        <p>Open source and free.</p>
                     </Typography.Title>
                     <Button
                         href="/graphs"
                         type="primary"
-                        status="success"
-                        shape="round"
                         size="large"
                         className="start-button"
+                        style={{
+                            fontSize: '2em',
+                            height: 'auto',
+                        }}
+                        shape="round"
                     >
-                        <strong>
-                            Get started free & no registration required.
-                        </strong>
+                        Get started
                     </Button>
+                </div>
+                <Steps
+                    labelPlacement="vertical"
+                    current={5}
+                    style={{
+                        maxWidth: '1200px',
+                        margin: '100px auto',
+                    }}
+                >
+                    <Step
+                        title="Design data structures"
+                        description="Visually"
+                    />
+                    <Step
+                        title="Create relationships"
+                        description="Drag and drop"
+                    />
+                    <Step
+                        title="Export SQL scripts"
+                        description="It's that simple"
+                    />
+                </Steps>
+                <div className="index-video-container">
+                    <div className="faq">
+                        <h2>FAQ</h2>
+                        <dl>
+                            <dt>How to register?</dt>
+                            <dd>
+                                No registration required, just start playing.
+                            </dd>
+                        </dl>
+                        <dl>
+                            <dt>How is the data saved?</dt>
+                            <dd>
+                                Saved in local storage and indexDB, so it's best
+                                to make a backup before cleaning the browser.
+                            </dd>
+                        </dl>
+                        <dl>
+                            <dt>
+                                Is it possible to collaborate across devices?
+                            </dt>
+                            <dd>Not for the time being.</dd>
+                        </dl>
+                    </div>
+                    <video src="/detail.mp4" muted autoPlay loop></video>
                 </div>
             </div>
         </>
