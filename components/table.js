d25b2a788df12776356b60ce23267cf50e8beea1
findyourmagic <findyourmagic@foxmail.com>
2022-09-04 11:07:16 +0800

    [fix] long table name not editable #23

--- components/table.js
+++ components/table.js
@@ -1,6 +1,20 @@
 import { useState } from 'react';
-import { Button, Popover, Space, Tag, Popconfirm, Input } from '@arco-design/web-react';
-import { IconEdit, IconDelete, IconMessage, IconPlus, IconMinus, IconPalette } from '@arco-design/web-react/icon';
+import {
+    Button,
+    Popover,
+    Space,
+    Tag,
+    Popconfirm,
+    Input,
+} from '@arco-design/web-react';
+import {
+    IconEdit,
+    IconDelete,
+    IconMessage,
+    IconPlus,
+    IconMinus,
+    IconPalette,
+} from '@arco-design/web-react/icon';
 
 import themes from '../data/theme';
 
@@ -44,14 +58,36 @@ export default function Table(props) {
             </div>
             <div className="content">
                 <Space wrap size={4}>
-                    {field.pk && <Tag size="small" color="gold">PRIMARY</Tag>}
-                    {field.unique && <Tag size="small" color="green">UNIQUE</Tag>}
-                    {field.not_null && <Tag size="small" color="magenta">NOT NULL</Tag>}
-                    {field.increment && <Tag size="small" color="lime">INCREMENT</Tag>}
+                    {field.pk && (
+                        <Tag size="small" color="gold">
+                            PRIMARY
+                        </Tag>
+                    )}
+                    {field.unique && (
+                        <Tag size="small" color="green">
+                            UNIQUE
+                        </Tag>
+                    )}
+                    {field.not_null && (
+                        <Tag size="small" color="magenta">
+                            NOT NULL
+                        </Tag>
+                    )}
+                    {field.increment && (
+                        <Tag size="small" color="lime">
+                            INCREMENT
+                        </Tag>
+                    )}
                 </Space>
 
-                <div className="field-item dbdefault"><span>DEFAULT:</span>{field.dbdefault}</div>
-                <div className="field-item note"><span>COMMENT:</span>{field.note}</div>
+                <div className="field-item dbdefault">
+                    <span>DEFAULT:</span>
+                    {field.dbdefault}
+                </div>
+                <div className="field-item note">
+                    <span>COMMENT:</span>
+                    {field.note}
+                </div>
             </div>
         </div>
     );
@@ -78,10 +114,11 @@ export default function Table(props) {
                 className={`table ${editable ? 'editable' : ''}`}
                 style={{ borderColor: table.theme }}
             >
-                <div className="table-title" style={{ background: table.theme }}>
-                    <span>
-                        {table.name}
-                    </span>
+                <div
+                    className="table-title"
+                    style={{ background: table.theme }}
+                >
+                    <span className="table-name">{table.name}</span>
 
                     {editable && (
                         <Space size={4}>
@@ -119,36 +156,55 @@ export default function Table(props) {
                                     setNote(props.table.note);
                                 }}
                             >
-                                <Button
-                                    size="mini"
-                                    icon={<IconMessage />}
-                                />
+                                <Button size="mini" icon={<IconMessage />} />
                             </Popconfirm>
                             <Popover
                                 position="tr"
                                 title={
                                     <>
                                         Theme
-
                                         <Button
                                             size="mini"
-                                            style={{ float: 'right', fontSize: '12px' }}
-                                            onClick={() => props.updateTable({ ...props.table, theme: undefined })}
+                                            style={{
+                                                float: 'right',
+                                                fontSize: '12px',
+                                            }}
+                                            onClick={() =>
+                                                props.updateTable({
+                                                    ...props.table,
+                                                    theme: undefined,
+                                                })
+                                            }
                                         >
                                             Clear
                                         </Button>
                                     </>
                                 }
                                 content={
-                                    <Space warp direction="vertical" size="medium" style={{ margin: '8px 0 4px' }}>
+                                    <Space
+                                        warp
+                                        direction="vertical"
+                                        size="medium"
+                                        style={{ margin: '8px 0 4px' }}
+                                    >
                                         {themes.map(list => (
-                                            <Space size="medium" key={list.toString()}>
+                                            <Space
+                                                size="medium"
+                                                key={list.toString()}
+                                            >
                                                 {list.map(item => (
                                                     <Button
                                                         key={item}
                                                         shape="circle"
-                                                        style={{ background: item }}
-                                                        onClick={() => props.updateTable({ ...props.table, theme: item })}
+                                                        style={{
+                                                            background: item,
+                                                        }}
+                                                        onClick={() =>
+                                                            props.updateTable({
+                                                                ...props.table,
+                                                                theme: item,
+                                                            })
+                                                        }
                                                     />
                                                 ))}
                                             </Space>
@@ -157,10 +213,7 @@ export default function Table(props) {
                                 }
                                 trigger="click"
                             >
-                                <Button
-                                    size="mini"
-                                    icon={<IconPalette />}
-                                />
+                                <Button size="mini" icon={<IconPalette />} />
                             </Popover>
                             <Popconfirm
                                 position="tr"
@@ -172,7 +225,7 @@ export default function Table(props) {
                                 }}
                             >
                                 <Button
-                                    status='danger'
+                                    status="danger"
                                     size="mini"
                                     icon={<IconDelete />}
                                 />
@@ -204,13 +257,29 @@ export default function Table(props) {
                                     <div>
                                         {field.name}
                                         {field.pk && (
-                                            <img style={{ width: 10, height: 10, marginLeft: 4 }} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAB30lEQVR4AYySg44eURxHm75MbdvmCzQso0a1bdsKatu2MTNre3fMT6c3t1gjyS+6Oef+1QpoMgVXR+4X4W/2131vEZwIs2UakjQPe5+JKo4Ttx8DKSnJuTD8aJOCmnBYso2o8tS/3wHIOdWPt/v6PWlCIMuWcFC4nLBsr8hDpOTicFIJnayD3aWkUYEoWcIyRaukJG49xEnfgpc1jmT4jPSdXWm8guCXqGATQlBDtBpbGYP5ua9o7zLK0nYNwq1F9pe/nCuAlVjfBuFlTMPPmYOjTsX41JO4sQ5lRTvur++p1d/39VGUv5pPTL8ofusn4Eni9xnYnwdivOsuYXVVBx5s7PUTwdUUiOFYxK0H+IVLML+3xfvZBz93DQXnBpNwToqyz6Gu7sCTLb2+/ONqXhpR+QUcbTLGuw44yiCCvM2839xdv7umR6ayrB3auo482977Q82W//5sipKv4moj0N+2xVWHEeRv4sOW7lXXN/Y909CgawrsmPEA61s/9Fdt8TMnC3gd77d0129t6XekUbhmBXH3NZYyAi9jLF7eat5u7Gbe2dbvYHOwFORdHP4pqHiJn7eCoPgE7zZ3N+7v7L+rJfDvgQ04Ndeu+uI0s6dnJhi/Bvn50FTrdpAEsRgA3aj/RFymC/IAAAAASUVORK5CYII=" />
+                                            <img
+                                                style={{
+                                                    width: 10,
+                                                    height: 10,
+                                                    marginLeft: 4,
+                                                }}
+                                                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAB30lEQVR4AYySg44eURxHm75MbdvmCzQso0a1bdsKatu2MTNre3fMT6c3t1gjyS+6Oef+1QpoMgVXR+4X4W/2131vEZwIs2UakjQPe5+JKo4Ttx8DKSnJuTD8aJOCmnBYso2o8tS/3wHIOdWPt/v6PWlCIMuWcFC4nLBsr8hDpOTicFIJnayD3aWkUYEoWcIyRaukJG49xEnfgpc1jmT4jPSdXWm8guCXqGATQlBDtBpbGYP5ua9o7zLK0nYNwq1F9pe/nCuAlVjfBuFlTMPPmYOjTsX41JO4sQ5lRTvur++p1d/39VGUv5pPTL8ofusn4Eni9xnYnwdivOsuYXVVBx5s7PUTwdUUiOFYxK0H+IVLML+3xfvZBz93DQXnBpNwToqyz6Gu7sCTLb2+/ONqXhpR+QUcbTLGuw44yiCCvM2839xdv7umR6ayrB3auo482977Q82W//5sipKv4moj0N+2xVWHEeRv4sOW7lXXN/Y909CgawrsmPEA61s/9Fdt8TMnC3gd77d0129t6XekUbhmBXH3NZYyAi9jLF7eat5u7Gbe2dbvYHOwFORdHP4pqHiJn7eCoPgE7zZ3N+7v7L+rJfDvgQ04Ndeu+uI0s6dnJhi/Bvn50FTrdpAEsRgA3aj/RFymC/IAAAAASUVORK5CYII="
+                                            />
                                         )}
                                         {(field.unique || field.index) && (
-                                            <img style={{ width: 10, height: 10, marginLeft: 4 }} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAA7UlEQVR4AW2Qg26GURQEb/rWtW3bts2otm27jdpouv39YTeeuTjH2FNdUo1a4gBB/KVaFQt+Zp9bkFJe5Yo3OKBahUJSZ60CX8KLbHMhoZIfsq1Kta5eVJel3LDIEA+kYxHeWBPwSn20ckkMQRxWXTLEEh1MMMsgzWwSS9Rh4Hs1jHJKJ0PM0U6jcDzRu4HLf7lmgUYJi5RzzLlw3EbI5o51aT3drJDwHnEaSyIJKyGnz+ijll7hxLfIRvt6vq9op4ZhlnU6ptK5Xh7oYZBF4j5jy4wzFWv6HrskfMTnG7dkRWbcJb8kvqWkGtf8ARG87I1KSSe4AAAAAElFTkSuQmCC" />
+                                            <img
+                                                style={{
+                                                    width: 10,
+                                                    height: 10,
+                                                    marginLeft: 4,
+                                                }}
+                                                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAA7UlEQVR4AW2Qg26GURQEb/rWtW3bts2otm27jdpouv39YTeeuTjH2FNdUo1a4gBB/KVaFQt+Zp9bkFJe5Yo3OKBahUJSZ60CX8KLbHMhoZIfsq1Kta5eVJel3LDIEA+kYxHeWBPwSn20ckkMQRxWXTLEEh1MMMsgzWwSS9Rh4Hs1jHJKJ0PM0U6jcDzRu4HLf7lmgUYJi5RzzLlw3EbI5o51aT3drJDwHnEaSyIJKyGnz+ijll7hxLfIRvt6vq9op4ZhlnU6ptK5Xh7oYZBF4j5jy4wzFWv6HrskfMTnG7dkRWbcJb8kvqWkGtf8ARG87I1KSSe4AAAAAElFTkSuQmCC"
+                                            />
                                         )}
                                     </div>
-                                    <div className="field-type">{field.type}</div>
+                                    <div className="field-type">
+                                        {field.type}
+                                    </div>
                                 </div>
                                 <div className="grip-setting">
                                     <Button
@@ -219,7 +288,10 @@ export default function Table(props) {
                                         size="mini"
                                         icon={<IconEdit />}
                                         onClick={() => {
-                                            handlerEditingField({ field, table });
+                                            handlerEditingField({
+                                                field,
+                                                table,
+                                            });
                                         }}
                                     />
                                     <Button
@@ -232,7 +304,7 @@ export default function Table(props) {
                                         }}
                                     />
                                     <Button
-                                        status='danger'
+                                        status="danger"
                                         className="grip-setting-btn"
                                         size="mini"
                                         icon={<IconMinus />}
